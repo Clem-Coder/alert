b4239a0df37a6aced1ee3e8c35daeb62c5caa6e7
Clément Autin <autin.clement60@gmail.com>
2021-12-01 17:28:00 +0100

    add firestationId class, to use the delete method of FirestationRepository

--- src/main/java/com/safetynet/alert/repository/FirestationRepository.java
+++ src/main/java/com/safetynet/alert/repository/FirestationRepository.java
@@ -1,17 +1,20 @@
 package com.safetynet.alert.repository;
 
 import com.safetynet.alert.model.Firestation;
+import com.safetynet.alert.model.idclasses.FirestationId;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-public interface FirestationRepository extends CrudRepository <Firestation, String> {
+public interface FirestationRepository extends CrudRepository <Firestation, FirestationId> {
 
     Iterable<Firestation> findAllByOrderByStationAsc();
 
     Iterable<Firestation> findByStation(int station);
 
     Iterable<Firestation> findByAddress(String address);
+
+
 }
